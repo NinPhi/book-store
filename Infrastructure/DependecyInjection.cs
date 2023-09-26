e0746c48ec621f8b907092bbe9f59eaf85441227
NinPhi <melmarka4@gmail.com>
2023-09-26 13:33:20 +0600

    Registered services

--- Infrastructure/DependecyInjection.cs
+++ Infrastructure/DependecyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Infrastructure.Data;
+﻿using Application.Shared;
+using Domain.Repositories;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,8 +15,36 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        return services
+            .AddAppDbContext(configuration)
+            .AddRepositories()
+            .AddOtherServices();
+    }
+
+    private static IServiceCollection AddAppDbContext(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
         services.AddDbContext<AppDbContext>(opts =>
             opts.UseSqlServer(configuration.GetConnectionString("Default")));
+
+        services.AddRepositories();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(
+        this IServiceCollection services)
+    {
+        services.AddTransient<IUserRepository, UserRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddOtherServices(
+        this IServiceCollection services)
+    {
+        services.AddTransient<IPasswordManager, PasswordManager>();
 
         return services;
     }

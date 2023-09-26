f737ba6f91cda9116bacfb2e79c8589189e3c31e
NinPhi <melmarka4@gmail.com>
2023-09-26 13:30:05 +0600

    Added user creation feature

---
+++ Application/Features/Users/Add.cs
@@ -0,0 +1,49 @@
+﻿using Application.Shared;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Repositories;
+using Domain.Shared;
+
+namespace Application.Features.Users;
+
+public record AddUserCommand : IRequest
+{
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    public required UserRole Role { get; init; }
+}
+
+internal class AddUserCommandHandler : IRequestHandler<AddUserCommand>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _uow;
+    private readonly IPasswordManager _passwordManager;
+
+    public AddUserCommandHandler(IUserRepository userRepository, IUnitOfWork uow, IPasswordManager passwordManager)
+    {
+        _userRepository = userRepository;
+        _uow = uow;
+        _passwordManager = passwordManager;
+    }
+
+    public async Task Handle(
+        AddUserCommand request, CancellationToken cancellationToken)
+    {
+        var userExists = await _userRepository.ExistsAsync(request.Username);
+        if (userExists) throw new Exception();
+
+        var passwordHash = _passwordManager.Hash(request.Password);
+
+        if (!Enum.IsDefined(request.Role)) throw new Exception();
+
+        var user = new User
+        {
+            Username = request.Username,
+            PasswordHash = passwordHash,
+            Role = request.Role,
+        };
+
+        _userRepository.Add(user);
+        await _uow.CommitAsync();
+    }
+}

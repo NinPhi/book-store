1080236a18957039971ff5fd66fb28f0639ff181
NinPhi <melmarka4@gmail.com>
2023-09-26 19:50:39 +0600

    Added book retrieval endpoints

--- Application/Features/Books/GetByTitle.cs
+++ Application/Features/Books/GetByTitle.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Contracts;
 using Domain.Repositories;
 using Mapster;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.Books;
 
---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.Books;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController, Route("api/books")]
+public class BookController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BookController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var request = new GetAllBooksQuery();
+
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{title}")]
+    public async Task<IActionResult> GetByTitle(string title)
+    {
+        var request = new GetBookByTitleQuery
+        {
+            Title = title,
+        };
+
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+}

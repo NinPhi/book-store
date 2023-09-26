2c6d6c2db219e71a252dcb46eb371dfdb01e87d7
NinPhi <melmarka4@gmail.com>
2023-09-26 19:25:08 +0600

    Added book and repository

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class Book
+{
+    public long Id { get; set; }
+
+    [StringLength(100)]
+    public required string ISBN { get; set; }
+
+    [StringLength(400)]
+    public required string Title { get; set; }
+
+    public int Count { get; set; }
+}
---
+++ Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories;
+
+internal class BookRepository : IBookRepository
+{
+    private readonly AppDbContext _context;
+
+    public BookRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Book>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book?> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book?> GetByTitle(string title)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Book entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Book entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Book entity)
+    {
+        throw new NotImplementedException();
+    }
+}

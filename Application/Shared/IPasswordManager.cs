452ea659e2db0b2923a1cf1d92d86276774d2c33
NinPhi <melmarka4@gmail.com>
2023-09-26 13:29:51 +0600

    Added password hashing

---
+++ Application/Shared/IPasswordManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Shared;
+
+public interface IPasswordManager
+{
+    string Hash(string password);
+    Task<bool> VerifyAsync(string username, string password);
+}
---
+++ Infrastructure/Services/PasswordManager.cs
@@ -0,0 +1,30 @@
+﻿using Application.Shared;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Services;
+
+internal class PasswordManager : IPasswordManager
+{
+    private readonly AppDbContext _context;
+
+    public PasswordManager(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password, 15);
+    }
+
+    public async Task<bool> VerifyAsync(string username, string password)
+    {
+        var hash = await _context.Users
+            .Where(u => u.Username == username)
+            .Select(u => u.PasswordHash)
+            .FirstOrDefaultAsync() ?? string.Empty;
+
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}

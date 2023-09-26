1bbeed6130df6a31419716875f02e1311ae1b454
NinPhi <melmarka4@gmail.com>
2023-09-26 13:11:39 +0600

    Added UoW

---
+++ Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task CommitAsync()
+    {
+        return _context.SaveChangesAsync();
+    }
+}

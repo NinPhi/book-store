c95bf65e850342880a6f1c8ebd8e40ff5f5ae56c
NinPhi <melmarka4@gmail.com>
2023-09-26 12:48:01 +0600

    Added AppDbContext

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data;
+
+internal class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
+    {
+        
+    }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}

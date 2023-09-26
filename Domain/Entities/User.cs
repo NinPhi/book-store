b3c0c21bbc764e4a422057ce0733dffd21664189
NinPhi <melmarka4@gmail.com>
2023-09-26 12:58:22 +0600

    Added user entity and role enum

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class User
+{
+    public long Id { get; set; }
+    public required string Username { get; set; }
+    public required string PasswordHash { get; set; }
+    public required UserRole Role { get; set; }
+}
